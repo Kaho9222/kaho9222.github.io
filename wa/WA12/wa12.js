8636d7d1b6501f65a71f91c0dbfe4f395d43af62
Kaho9222 <kaho9222@colorado.edu>
2024-04-14 22:06:37 -0600

    wa12

---
+++ wa/WA12/wa12.js
@@ -0,0 +1,34 @@
+const btn= document.querySelector("#js-dog-button");
+const image=document.querySelector('#js-dog-image');
+if(image.style.height >= image.style.width) {
+    image.style.height = '40opx'
+    } else {
+    image.style.width = '400px'
+    }
+
+
+
+//const answerbtn= document.querySelector("#js-a");
+//answerbtn.addEventListener('click',getAnswer);
+
+//const answerText=document.querySelector("#js-answer-text");
+btn.addEventListener('click',getDog);
+    
+
+
+async function getDog(){
+   try {
+    const response = await fetch ("https://dog.ceo/api/breeds/image/random");  
+
+        const jsonData = await response.json();
+
+        console.log(jsonData);
+        image.src=jsonData['message'];
+    }      
+
+    catch(error){
+        console.log('failed to load');
+    }
+}
+
+getDog();
